19e8d10e0481854d323893e17bce9366ffe74d61
MichalR90 <michalrutkowski90@wp.pl>
2025-06-07 09:34:32 +0200

    changed background

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -1,8 +1,20 @@
 import "../styles/MainLayout.css";
+import background from "../assets/background.jpg";
 function MainLayout({ children }) {
   return (
     <>
-      <div className="main-layout">{children}</div>
+      <div
+        style={{
+          backgroundImage: `url(${background})`,
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+          minHeight: "100vh",
+        }}
+        className="main-layout"
+      >
+        {children}
+      </div>
     </>
   );
 }
